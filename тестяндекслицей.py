8a12635f051fe0966169d553c50e186da534600d
k7ndcat <peterkhrapov@gmail.com>
2025-02-14 23:11:31 +0300

    Update тестяндекслицей.py
    Теперь для 3го задания сделал импорт через ui.py

--- тестяндекслицей.py
+++ тестяндекслицей.py
@@ -1,18 +1,40 @@
 import sys
 import random
 from PyQt6 import uic
+from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
 from PyQt6.QtGui import QPainter, QColor
 
 
-class MyWidget(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(240, 320)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(15, 106, 161, 71))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "круги"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
-
-        self.pushButton.clicked.connect(self.add_circle)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.add_circle)
         self.circles = []
-
+        
     def paintEvent(self, event):
         painter = QPainter(self)
         for circle in self.circles:
